b15e25abcf53b3a05b4be4ef9ca5a563bd50ea83
Chris Jennison <cjennison92@gmail.com>
2018-05-31 20:54:56 -0400

    upgraded to react 16; added material

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,36 +3,63 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { signOutAction } from 'store/actions/signin';
 
-class Navbar extends Component {
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import KeyIcon from '@material-ui/icons/VpnKey';
+import ExitIcon from '@material-ui/icons/ExitToApp';
 
+class Navbar extends Component {
   signout = () => {
     this.props.dispatch(signOutAction())
   }
 
-  navbarLinks() {
+  authDisplay() {
     if (this.props.user) {
-      return [
-        <li key="secret"><Link to="/counter">Secret</Link></li>,
-        <li key="signout"><Link to="#" onClick={ this.signout }>Sign out</Link></li>,
-        <li key="email">{ this.props.user.email }</li>
-      ];
+      return (
+        <span>{this.props.user.email}</span>
+      )
     }
-    return [
-      <li key="signin"><Link to="/auth/signin">Sign in</Link></li>,
-      <li key="signup"><Link to="/auth/register">Sign up</Link></li>,
-    ];
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <nav className="navbar">
-        <div className="container">
-          <Link to="/"><span className="brand">Home</span></Link>
-          <ul>
-            {this.navbarLinks()}
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <h4 style={{ textAlign: "center" }}>{this.authDisplay()}</h4>
+        <Divider />
+        <List>
+          <Link to="/">
+            <ListItem button href="/">
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </Link>
+          <Link to="/auth">
+            <ListItem button>
+              <ListItemIcon>
+                <KeyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Auth" />
+            </ListItem>
+          </Link>
+          <Divider />
+          {this.props.user ? (
+            <ListItem button onClick={this.signout}>
+              <ListItemIcon>
+                <ExitIcon />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItem>
+          ) : null}
+        </List>
+      </div>
     );
   }
 }
--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -2,25 +2,162 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
 import NavBar from 'components/NavBar'
+
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
+import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+
 import './PageLayout.scss'
 
-export const PageLayout = ({ children }) => (
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: "100%",
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class PageLayout extends React.Component {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  render(children) {
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <NavBar />
+    )
+
+    return (
+      <div className={classes.root}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {this.props.children}
+        </main>
+      </div>
+
+    );
+  }
+}
+
+PageLayout.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(PageLayout);
+
+
+
+
+
+export const _PageLayout = ({ children }) => (
   <div>
-    <NavBar />
-    <div className='container text-center'>
-      <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-      {' · '}
-      <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
-      {' · '}
-      <Link to='/auth' activeClassName='page-layout__nav-item--active'>Sign Up</Link>
-      <div className='page-layout__viewport'>
-        {children}
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <div>
+          <div className='page-layout__viewport'>
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   </div>
 )
-PageLayout.propTypes = {
-  children: PropTypes.node,
-}
 
-export default PageLayout
+
+/*PageLayout.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+}*/
+
+//export default withStyles(styles, { withTheme: true })(PageLayout);
